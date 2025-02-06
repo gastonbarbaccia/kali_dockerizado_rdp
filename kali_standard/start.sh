6b1fe716e4ad2e61cb63d03ac102e710672529da
Gaston Barbaccia <gastonbarbaccia@devcodeacademy.com>
2025-02-05 21:52:44 -0300

    ok

---
+++ kali_standard/start.sh
@@ -0,0 +1,11 @@
+#bin/bash
+
+sudo apt-get update -y
+
+sudo apt-get upgrade -y
+
+sudo apt-get install docker.io -y
+
+sudo docker build -t kali-xrdp .
+
+sudo docker run -d -p 3389:3389 -p 5432:5432 --name kali -v kali_data:$(pwd) kali-xrdp
